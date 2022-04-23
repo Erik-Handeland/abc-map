ba9f071ae51f2248fd485af9b3892672e75da516
Erik-Handeland <erikhandeland@gmail.com>
2022-04-22 20:05:57 -0700

    remove duplicates

--- packages/frontend/scripts/test-setup 2.js
+++
@@ -1,7 +0,0 @@
-const sinon = require('sinon');
-
-global.URL.createObjectURL = sinon.stub();
-window.URL.createObjectURL = sinon.stub();
-
-global.fetch = sinon.stub();
-window.fetch = sinon.stub();
